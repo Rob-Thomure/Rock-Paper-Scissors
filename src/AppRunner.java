cdfde8dc55411e89042aed5cd3510166e979dc3a
Rob Thomure <robthomure@outlook.com>
2023-04-16 14:14:19 -0500

    stage 3: endless game

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -8,21 +8,26 @@ public class AppRunner {
     }
 
     public void runApp() {
-        String humanHandSign = userInterface.getSignFromConsole();
-        String computerHandSign = computerPlayer.chooseSign();
-        GameResults gameResults = GameResults.getGameResults(HandSigns.valueOf(humanHandSign.toUpperCase())
-                , HandSigns.valueOf(computerHandSign.toUpperCase()));
-        printResults(gameResults, computerHandSign);
+        boolean quit = false;
+        while (!quit) {
+            String humanHandSign = userInterface.getSignFromConsole();
+            if (!humanHandSign.equalsIgnoreCase("!exit")) {
+                String computerHandSign = computerPlayer.chooseSign();
+                GameResults gameResults = GameResults.getGameResults(HandSigns.valueOf(humanHandSign.toUpperCase())
+                        , HandSigns.valueOf(computerHandSign.toUpperCase()));
+                printResults(gameResults, computerHandSign);
+            } else {
+                quit = true;
+            }
+        }
+        userInterface.printBye();
     }
 
-    public void printResults(GameResults gameResults, String computerHandSign) {
+    private void printResults(GameResults gameResults, String computerHandSign) {
         switch (gameResults) {
             case WIN -> userInterface.printWin(computerHandSign);
             case LOSS -> userInterface.printLoss(computerHandSign);
             case DRAW -> userInterface.printDraw(computerHandSign);
         }
     }
-
-
-
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,21 +1,42 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserInterface {
 
     public String getSignFromConsole() {
+        String validInput = "(!exit)|(rock)|(paper)|(scissors)";
+        Pattern pattern = Pattern.compile(validInput, Pattern.CASE_INSENSITIVE);
+        String input = "";
         Scanner scanner = new Scanner(System.in);
-        return scanner.next();
+        boolean isValidInput = false;
+        while (!isValidInput) {
+            input = scanner.next();
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.matches()) {
+                isValidInput = true;
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
+        return input;
     }
 
     public void printWin(String computerSign) {
-        System.out.printf("Well done. The computer chose %s and failed", computerSign);
+        System.out.printf("Well done. The computer chose %s and failed\n", computerSign);
     }
 
     public void printDraw(String computerSign) {
-        System.out.printf("There is a draw (%s)", computerSign);
+        System.out.printf("There is a draw (%s)\n", computerSign);
     }
 
     public void printLoss(String computerSign) {
-        System.out.printf("Sorry, but the computer chose %s", computerSign);
+        System.out.printf("Sorry, but the computer chose %s\n", computerSign);
     }
+
+    public void printBye() {
+        System.out.println("Bye!");
+    }
+
+
 }
